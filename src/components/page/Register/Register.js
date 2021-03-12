73d607da7d9f6cf1ba12ed0b68ef0402967d9725
Jeremy Ng <resumisg@gmail.com>
2021-03-12 11:40:39 +0800

    changed class component to functional hook component

--- src/components/page/Register/Register.js
+++ src/components/page/Register/Register.js
@@ -1,132 +1,131 @@
 import axios from "axios";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Modal, Form, Button } from "react-bootstrap";
 import { useHistory } from "react-router";
 
-// TODO: convert this class component into a function hook component
-export default class Register extends Component {
-  state = {
-    username: "",
-    password1: "",
-    password2: "",
-    success: false,
-    error: null,
-  };
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
 
-  validateForm = () => {
+  const history = useHistory();
+
+  const validateForm = () => {
     return (
-      this.state.username.length > 0 &&
-      this.state.password1 === this.state.password2 &&
-      this.state.password1.length > 0
+      username.length > 0 && password1 === password2 && password1.length > 0
     );
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     const data = axios
       .post("http://localhost:5000/register", {
-        username: this.state.username,
-        password: this.state.password1,
+        username: username,
+        password: password1,
       })
       .then((response) => {
         console.log("successful registration!");
-        this.setState({ success: true });
+        setSuccess(true);
         return;
       })
       .catch((error) => {
         console.log("TODO: catch error", error.response);
-        this.setState({ error: JSON.stringify(error, null, 1) });
+        setError(JSON.stringify(error, null, 1));
         return error;
       });
   };
 
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        {/* Error modal */}
-        <Modal show={this.state.error != null}>
-          <Modal.Header>
-            <Modal.Title>Error</Modal.Title>
-          </Modal.Header>
+  return (
+    <Form onSubmit={handleSubmit}>
+      {/* Error modal */}
+      <Modal show={error != null}>
+        <Modal.Header>
+          <Modal.Title>Error</Modal.Title>
+        </Modal.Header>
 
-          <Modal.Body>
-            <p>{this.state.error != null ? this.state.error : null}</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button
-              variant="danger"
-              onClick={() => {
-                this.setState({ error: null });
-              }}
-            >
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <Modal.Body>
+          <p>{error != null ? error : null}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="danger"
+            onClick={() => {
+              setError(null);
+            }}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-        {/* success banner, only shows if attained sugarword */}
-        <Modal show={this.state.success}>
-          <Modal.Header>
-            <Modal.Title>Logged in!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>User logged in!</Modal.Body>
-          <Modal.Footer>
-            <Button
-              variant="success"
-              onClick={() => {
-                console.log(this);
-                //
-                // this.props.history.push({
-                //   pathname: "/",
-                //   state: { username: this.state.username },
-                // });
-              }}
-            >
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
+      {/* success banner, only shows if attained sugarword */}
+      <Modal show={success}>
+        <Modal.Header>
+          <Modal.Title>Logged in!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>User logged in!</Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="success"
+            onClick={() => {
+              history.push({
+                pathname: "/",
+                state: { username: username },
+              });
+              // console.log(this);
+              //
+              // this.props.history.push({
+              //   pathname: "/",
+              //   state: { username: username },
+              // });
+            }}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-        <h3>Register</h3>
-        <Form.Group>
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            autoFocus
-            type="username"
-            placeholder="Enter username"
-            value={this.state.username}
-            onChange={(e) => this.setState({ username: e.target.value })}
-          />
-        </Form.Group>
+      <h3>Register</h3>
+      <Form.Group>
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          autoFocus
+          type="username"
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            value={this.state.password1}
-            onChange={(e) => this.setState({ password1: e.target.value })}
-          />
-        </Form.Group>
+      <Form.Group>
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Enter password"
+          value={password1}
+          onChange={(e) => setPassword1(e.target.value)}
+        />
+      </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Confirmed password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password again"
-            value={this.state.password2}
-            onChange={(e) => this.setState({ password2: e.target.value })}
-          />
-        </Form.Group>
+      <Form.Group>
+        <Form.Label>Confirmed password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Enter password again"
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
+        />
+      </Form.Group>
 
-        <Button block size="lg" type="submit" disabled={!this.validateForm()}>
-          Register
-        </Button>
-        <p className="already-register text-right">
-          Already registered <a href="/login">sign in?</a>
-        </p>
-      </Form>
-    );
-  }
+      <Button block size="lg" type="submit" disabled={!validateForm()}>
+        Register
+      </Button>
+      <p className="already-register text-right">
+        Already registered <a href="/login">sign in?</a>
+      </p>
+    </Form>
+  );
 }
